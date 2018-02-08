f4317b49ff698485ffa2a6e3922b250408ba6389
dallas-legrande <33949897+dallas-legrande@users.noreply.github.com>
2018-02-08 12:12:25 -0700

    completed Node for Huffman Code

--- Node.java
+++ Node.java
@@ -1,13 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package huffmancode;
 
 /**
- *
- * @author Dallas
+ * Authors: Dallas LeGrande, Selene Smith
+ * Date: 2/6/18
+ * 
+ * Overview: Program #1 - Huffman Codes
+ * Program used to:
+ * 1. Accept a text message via input file (/input/input.txt)
+ * 2. Construct frequency table for characters in the message
+ * 3. Create a Huffman Tree for the message
+ * 4. Create a code table from the Huffman Tree
+ * 5. Encode the original message into binary
+ * 6. Decode the encoded message and write the decoded message as output
+ *      to an output file (/output/output.txt)
+ * 
+ * Credit to Robert Lafore, Data Structures and Algorithms in Java (2 ed.)
+ * for the TreeApp.java code used in parts of this program
+ * 
  */
 
 public class Node implements Comparable<Node>{
@@ -35,6 +44,8 @@ public class Node implements Comparable<Node>{
         return frequency;
     }
 
+    // used with the priority queue
+    // this tells the queue to base the priority on the node with the lowest frequency
     @Override
     public int compareTo(Node o) {
         return frequency - o.frequency;
